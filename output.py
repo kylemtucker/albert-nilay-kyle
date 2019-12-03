5c12fa847205ae251bf87075bad92959aadde11d
Kyle Tucker <kyletucker@berkeley.edu>
2019-12-02 19:27:10 -0800

    Skeleton

--- output.py
+++
@@ -1,5 +0,0 @@
-# File for processing output paths and exporting with proper format to .out files
-# Inputs: Array of points charting our tour from soda hall.
-#         Array of equal length, where indices refer to stops above. At each index, value of array refers to which individuals have been dropped off here
-# Output: Writes to .out file with proper formatting
-#         Writes to .log file with information regarding the energy exerted along this path!
--- pasrser.py
+++
@@ -1,7 +0,0 @@
-# Parser tools for taking in .in files and convering into python data structures
-
-def parse_input(filename):
-	f = open(filename, '+r')
-	# Read the file, put into 2D array
-	# Generate graph if we choose to, may be easier to work with graph rep for things like djikstras or BFS/DFS
-
